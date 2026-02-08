3ff2fd9c8311a4d96c8a63e5cd531b12dec3363b
tooj <tjnyaina@gmail.com>
2026-02-08 15:32:14 +0300

    first verse

---
+++ src/main/kotlin/org/example/demo/LyricsXmasDay.kt
@@ -0,0 +1,10 @@
+package org.example.demo
+
+class LyricsXmasDay {
+    fun verse(verseNumber: Int): String {
+        return """
+                On the first day of Christmas
+                My true love gave to me:
+            """
+    }
+}
---
+++ src/test/kotlin/org/example/demo/LyricsXmasDayTest.kt
@@ -0,0 +1,25 @@
+package org.example.demo
+
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class LyricsXmasDayTest {
+    lateinit var lyrics: LyricsXmasDay
+
+    @BeforeTest
+    fun setUp() {
+        lyrics = LyricsXmasDay()
+    }
+
+    @Test
+    fun `first two lines of first verse`() {
+        assertEquals(
+            """
+                On the first day of Christmas
+                My true love gave to me:
+            """,
+            lyrics.verse(1)
+        )
+    }
+}
